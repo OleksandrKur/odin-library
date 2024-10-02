5e2160d39d5be4069114253e06553c1a10e31e54
OleksandKur <136488587+OleksandrKur@users.noreply.github.com>
2024-10-02 12:52:37 +0300

    Add library class, add input constructor add Form and Display modules

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-const myLibrary = [];
-const table = document.getElementById("book-list").lastElementChild
-const titleInput = document.getElementById("title")
-const authorInput = document.getElementById("author")
-const pagesInput = document.getElementById("pages")
-const readInput = document.getElementById("read")
-const submitButton = document.getElementById("sumbit-button")
-const changeStatusButtons = document.getElementsByClassName("changeStatus");
-const deleteEntryButtons = document.getElementsByClassName("deleteBook");
-let idCount = 1;
 
 class Book{
   constructor(title, author, pages, read){
@@ -15,97 +5,148 @@ class Book{
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.id = idCount++;
+    this.id = null;
+  }
+  changeReadStatus(){
+    this.read = this.read === true ? false : true;
+  }
+  assignId(id){
+    this.id = id;
   }
 }
 
-submitButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  processForm();
-})
+class Library{
+  constructor(){
+    this.library = [];
+    this.idCount = 0;
+  }
+  addBook(book){
+    this.library.push(book);
+    this.idCount++;
+    book.assignId(this.idCount);
+  }
+  removeBookById(id){
+    let bookIndex = this.library.findIndex((element) => element.id === id); 
+    this.library.splice(bookIndex, 1);
+    console.log(this.library);
+  }
+  changeReadStatusById(id){
+    let bookIndex = this.library.findIndex((element) => element.id === id); 
+    this.library[bookIndex].changeReadStatus(bookIndex)
+    console.log(this.library[bookIndex]);
+  }
 
-function addBookToLibrary(book) {
-
-  myLibrary.push(book);
-  const lastBook = myLibrary[myLibrary.length - 1];
-
-  addRow(lastBook);
-
-  function addRow(lastBook){
-    let tableRow = document.createElement("tr");
-    tableRow.classList.add("table-row");
-    table.appendChild(tableRow);
-    addCell(lastBook.title);
-    addCell(lastBook.author);
-    addCell(lastBook.pages);
-    addCell(lastBook.read ? "Finished" : "Not finished");
-    createButton("🔄", lastBook.id, "changeStatus", changeStatus);
-    createButton("🚮", lastBook.id, "deleteBook", removeBook);
+}
 
 
-    function addCell(text){
-      let newCell = document.createElement("td");
-      newCell.textContent = text;
-      table.lastChild.appendChild(newCell);
+const Form = (function(){
+
+  const titleInput = document.getElementById("title");
+  const authorInput = document.getElementById("author");
+  const pagesInput = document.getElementById("pages");
+  const readInput = document.getElementById("read");
+  const submitButton = document.getElementById("sumbit-button");
+  submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    processForm();
+  })
+
+  function processForm(){
+    const title = titleInput.value; 
+    const author = authorInput.value;
+    const pages = pagesInput.value;  
+    const read = readInput.checked ? true : false;
+    if (title === "" || author === "" || pages === ""){
+      alert("Need to fill all inputs");
+      return;
     }
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.addBook(newBook);
+    Display.addBookToPage(newBook);
+    clearForm();
+  } 
 
-    function createButton(text, id, cssClass, buttonFunction){
-      let buttonCell = document.createElement("td");
-      let button = document.createElement("button");
-      button.classList.add(cssClass);
-      button.classList.add("button-small");
-      button.innerHTML = text;
-      button.setAttribute("data-id", id)
-      button.onclick = function(event){
-        buttonFunction(event.target);
-        event.preventDefault();
+  function clearForm(){
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    readInput.checked = false;
+  }
+})()
+
+const myLibrary = new Library();
+const Display = (function(){
+  const table = document.getElementById("book-list").lastElementChild
+
+  function addBookToPage(book) {
+  
+    addRow(book);
+  
+    function addRow(book){
+      let tableRow = document.createElement("tr");
+      tableRow.classList.add("table-row");
+      table.appendChild(tableRow);
+      addCell(book.title);
+      addCell(book.author);
+      addCell(book.pages);
+      addCell(book.read ? "Finished" : "Not finished");
+      createButton("🔄", book, "changeStatus", changeStatus);
+      createButton("🚮", book, "deleteBook", removeBookFromPage);
+  
+  
+      function addCell(text){
+        let newCell = document.createElement("td");
+        newCell.textContent = text;
+        table.lastChild.appendChild(newCell);
       }
-      table.lastChild.appendChild(buttonCell);
-      buttonCell.appendChild(button);
+  
+      function createButton(text, book, cssClass, buttonFunction){
+        let buttonCell = document.createElement("td");
+        let button = document.createElement("button");
+        button.classList.add(cssClass);
+        button.classList.add("button-small");
+        button.innerHTML = text;
+        button.setAttribute("data-id", book.id)
+        button.onclick = function(event){
+          buttonFunction(event.target);
+          event.preventDefault();
+        }
+        table.lastChild.appendChild(buttonCell);
+        buttonCell.appendChild(button);
+      }
     }
-  }
-  
-}
-
-function processForm(){
-
-  const title = titleInput.value; 
-  const author = authorInput.value;
-  const pages = pagesInput.value;  
-  const read = readInput.checked ? true : false;
-
-  if (title === "" || author === "" || pages === ""){
-    alert("Need to fill all inputs");
-    return;
+    
   }
 
-  const newBook = new Book(title, author, pages, read);
-  addBookToLibrary(newBook);
+  function removeBookFromPage(removeButton){
+    let currentBookId = Number(removeButton.getAttribute("data-id"));
+    myLibrary.removeBookById(currentBookId);
 
-  clearForm();
-}
-
-function clearForm(){
-  titleInput.value = "";
-  authorInput.value = "";
-  pagesInput.value = "";
-  readInput.checked = false;
-}
-
-function removeBook(removeButton){
-  let currentBookId = Number(removeButton.getAttribute("data-id"));
+    let currentRow = removeButton.parentElement.parentElement;
+    currentRow.remove();
+  }
   
-  let currentBookIndex = myLibrary.findIndex((element) => element.id === currentBookId); 
-  myLibrary.splice(currentBookIndex, 1);
+  function changeStatus(changeButton){
+    let currentBookId = Number(changeButton.getAttribute("data-id"));
+    myLibrary.changeReadStatusById(currentBookId);
+    let readStatusCell = changeButton.parentElement.previousSibling;
 
-  let currentRow = removeButton.parentElement.parentElement;
-  currentRow.remove();
-}
+    readStatusCell.textContent = readStatusCell.textContent == "Not finished" ? "Finished" : "Not finished"
+  }
 
-function changeStatus(changeButton){
-  let currentBookId = Number(changeButton.getAttribute("data-id"));
-  let currentBookIndex = myLibrary.findIndex((element) => element.id === currentBookId);
-  myLibrary[currentBookIndex].read = myLibrary[currentBookIndex].read == true ? false : true;
-  let readStatusCell = changeButton.parentElement.previousSibling;
-  readStatusCell.textContent = readStatusCell.textContent == "Not finished" ? "Finished" : "Not finished"
-}
+
+  return{
+    addBookToPage,
+    removeBookFromPage,
+  }
+})()
+
+
+
+
+
+
+
+
+
+
